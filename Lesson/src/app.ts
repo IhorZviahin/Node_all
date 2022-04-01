b0204a69e589835cf4c0b80a985c43e12d9c2cbe
IhorZviahin <igorzvjagin26@gmail.com>
2022-04-01 21:39:56 +0300

    help

--- Lesson/src/app.ts
+++ Lesson/src/app.ts
@@ -1,7 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+
+import { users } from './users';
 
 const app = express();
+console.log(users);
+app.get('/', (req: Request, res: Response) => {
+    res.end();
+});
 
-app.listen(5628, ()=>{
+app.listen(5500, () => {
     console.log('Hi');
-} );
+});
